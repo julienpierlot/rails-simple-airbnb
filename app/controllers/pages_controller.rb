9e0e676edeefb3ff209a9eb2304835c1535ecdb8
Julien Pierlot <pierlot.julien@gmail.com>
2020-02-12 19:05:33 +0100

    solve exo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def search
     @flats = Flat.where('name LIKE ?', params[:keywords])
+    if @flats.empty?
+      flash[:notice] = "No results sorry"
+      redirect_to flats_path
+    end
   end
 end
